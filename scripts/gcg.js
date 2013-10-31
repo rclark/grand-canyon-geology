a77f3e910e9bc8bf5a2de9639aafae357f269108
Ryan Clark <ryan.clark.j@gmail.com>
2013-10-30 19:09:36 -0700

    try hard-wired retina

--- scripts/gcg.js
+++ scripts/gcg.js
@@ -7,11 +7,13 @@ var gcg = {
   })
 };
 
-L.mapbox.tileLayer('http://a.tiles.usgin.org/grand-canyon/tiles.json', {
+L.mapbox.tileLayer('http://a.tiles.usgin.org/grand-canyon-retina/tiles.json', {
   retinaVersion: 'http://a.tiles.usgin.org/grand-canyon-retina/tiles.json'
 }).addTo(gcg.map);
 
+/*
 L.mapbox.tileLayer('rclark.map-55q8nfsk', {
   attribution: '<a href="https://www.mapbox.com/about/maps/">Terms & Feedback</a>',
   detectRetina: true
 }).addTo(gcg.map);
+*/
