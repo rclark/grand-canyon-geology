7e312b9c1581c709678e6501d5dcbf48f6f526fc
Ryan Clark <ryan.clark.j@gmail.com>
2013-10-30 17:11:32 -0700

    scrolling cleanup

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,17 +1,9 @@
-$('#info').waypoint(scrolling, { offset: '50%' });
-$('.scroll-btn').click(scrolling);
+$('.scroll-btn').click(function (evt) {
+  var position = $(window).scrollTop() === 0 ? $('#info').position().top : 0;
+  $("html, body").animate({scrollTop: position});
+});
 
-function scrolling(direction) {
-  if (typeof direction !== 'string') direction = $(window).scrollTop() === 0 ? 'down': 'up';
-
-  var position = direction === 'up' ? 0 : $('#info').position().top,
-      btnText = position === 0 ? 'About this map' : 'View the map',
-      hash = position === 0 ? '#map' : '#info';
-
-  $("html, body").animate({scrollTop: position}, function () {
-    var i = $('#mover').find('i');
-
-    window.location.hash = hash;
-    $('#mover').text(btnText);
-  });
-}
+$('#info').waypoint(function (direction) {
+  var position = direction === 'up' ? 0 : $('#info').position().top;
+  $("html, body").animate({scrollTop: position});
+}, { offset: '50%' });
